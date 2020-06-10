c825421328e9dfc1c454c58d1187d8bad5dd0e31
Steffen Leistner <sleistner@gmail.com>
2020-06-10 15:28:51 +0200

    feat: expose config

---
+++ index.js
@@ -0,0 +1,63 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        sourceType: "module",
+        project: "./tsconfig.json",
+    },
+    env: {
+        node: true,
+        es6: true,
+    },
+    extends: ["plugin:@typescript-eslint/recommended", "prettier", "prettier/@typescript-eslint"],
+    plugins: ["import", "prettier", "@typescript-eslint"],
+    globals: {},
+    rules: {
+        // prettier
+        "prettier/prettier": ["error"],
+        // TypeScript
+        "@typescript-eslint/no-unused-vars": "error",
+        "@typescript-eslint/explicit-member-accessibility": "off",
+        "@typescript-eslint/no-object-literal-type-assertion": "off",
+        // import
+        "import/extensions": [
+            "error",
+            "ignorePackages",
+            {
+                js: "never",
+                mjs: "never",
+                ts: "never",
+            },
+        ],
+        indent: ["error", 4],
+        "no-useless-constructor": "off",
+    },
+    overrides: [
+        {
+            files: ["**/*.spec.ts", "test/**/*.ts"],
+            env: {
+                jest: true,
+            },
+            rules: {
+                "import/named": "off",
+                "import/no-extraneous-dependencies": "off",
+                "max-classes-per-files": "off",
+                "global-require": "off",
+                "@typescript-eslint/no-var-requires": "off",
+            },
+        },
+        {
+            files: ["test/**/*.ts"],
+            rules: {
+                "no-param-reassign": "off",
+            },
+        },
+    ],
+    settings: {
+        "import/resolver": {
+            node: {
+                extensions: [".js", ".ts", ".json"],
+            },
+        },
+        "import/extensions": [".js", ".ts", ".mjs"],
+    },
+};
